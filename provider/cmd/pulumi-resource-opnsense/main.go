14800b033956c0bd28354944cc9ec06bf19a8292
Marc Ende <380468+eBeyond@users.noreply.github.com>
2025-01-28 21:53:09 +0100

    ci: disabled pulumi up/down (#5929)
    feat: release

--- provider/cmd/pulumi-resource-opnsense/main.go
+++ provider/cmd/pulumi-resource-opnsense/main.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2022, Pulumi Corporation.
+// Copyright 2016-2023, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,63 +15,10 @@
 package main
 
 import (
-	"fmt"
-	"github.com/oss4u/pulumi-opnsense-native/cmd/pulumi-resource-opnsense/core/config"
-	"github.com/oss4u/pulumi-opnsense-native/cmd/pulumi-resource-opnsense/core/unbound"
 	p "github.com/pulumi/pulumi-go-provider"
-	"github.com/pulumi/pulumi-go-provider/infer"
-	"github.com/pulumi/pulumi-go-provider/middleware/schema"
-	"os"
+
+	opnsense "github.com/oss4u/pulumi-opnsense/provider"
 )
 
-// Version is initialized by the Go linker to contain the semver of this build.
-var Version string
-
-func main() {
-	err := p.RunProvider("opnsense", Version, provider())
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %s", err.Error())
-		os.Exit(1)
-	}
-}
-
-func provider() p.Provider {
-	prv := infer.Provider(infer.Options{
-		Metadata: schema.Metadata{
-			DisplayName: "Opnsense",
-			License:     "Apache-2.0",
-			Repository:  "https://github.com/oss4u/pulumi-opnsense-native",
-			Publisher:   "Oss4u",
-			LanguageMap: map[string]any{
-				"nodejs": map[string]any{
-					"packageName": "@oss4u/opnsense",
-				},
-				"go": map[string]any{
-					"generateResourceContainerTypes": true,
-					"importBasePath":                 "github.com/oss4u/pulumi-opnsense-native/sdk/go/opnsense",
-				},
-				"csharp": map[string]any{
-					"rootNamespace": "Oss4u",
-				},
-			},
-			PluginDownloadURL: "github://api.github.com/oss4u/pulumi-opnsense-native",
-		},
-		Resources: []infer.InferredResource{
-			infer.Resource[unbound.HostAliasOverride, unbound.HostAliasOverrideArgs, unbound.HostAliasOverrideState](),
-			infer.Resource[unbound.HostOverride, unbound.HostOverrideArgs, unbound.HostOverrideState](),
-		},
-		Config: infer.Config[*config.Config](),
-	})
-	prv.DiffConfig = diff()
-	return prv
-}
-
-func diff() func(ctx p.Context, req p.DiffRequest) (p.DiffResponse, error) {
-	return func(ctx p.Context, req p.DiffRequest) (p.DiffResponse, error) {
-		return p.DiffResponse{
-			DeleteBeforeReplace: false,
-			HasChanges:          false,
-			DetailedDiff:        nil,
-		}, nil
-	}
-}
+// Serve the provider against Pulumi's Provider protocol.
+func main() { p.RunProvider(opnsense.Name, opnsense.Version, opnsense.Provider()) }
